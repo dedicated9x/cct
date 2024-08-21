4ce5bae94309d8b955eb006115e2328ba58c2654
driven12 <pawel.brysch.2@proton.me>
2024-08-21 17:04:59 +0200

    logic of transform_y has been implemented, not tested

--- src/tasks/gsn1/dataset.py
+++ src/tasks/gsn1/dataset.py
@@ -7,11 +7,24 @@ from sklearn.model_selection import train_test_split
 from torchvision import transforms
 
 class Augmentation:
-    def __init__(self, transform_x, transform_y):
+    def __init__(self, transform_x, transform_y_scheme):
         self.transform_x = transform_x
-        self.transform_y = transform_y
+        self.transform_y_scheme = transform_y_scheme
 
-def get_transform(config: omegaconf.DictConfig, split: str):
+    def transform_y(self, y):
+        base_scheme = ['squares', 'circles', 'up', 'right', 'down', 'left']
+
+        new2old = {k: v for k, v in zip(self.transform_y_scheme, base_scheme)}
+
+        dict_counts_orig = {k: v for k, v in zip(base_scheme, y)}
+        dict_counts = {k: dict_counts_orig[new2old[k]] for k in base_scheme}
+
+        y = list(dict_counts.values())
+        return y
+
+
+
+def get_augs(config: omegaconf.DictConfig, split: str):
     assert split in ["train", "val", "test"]
 
     # rotation_degrees = [90, 180, 270]
@@ -23,9 +36,10 @@ def get_transform(config: omegaconf.DictConfig, split: str):
     #     transforms.RandomHorizontalFlip(p=1.0)
     # ])
 
-    _transform = transforms.RandomRotation(degrees=(90, 90))
-
-    return _transform
+    dict_transforms = {
+        "rotation_90": Augmentation(transforms.RandomRotation(degrees=(90, 90)), ['squares', 'circles', 'left', 'up', 'right', 'down'])
+    }
+    return dict_transforms
 
 
 class ImagesDataset(torch.utils.data.Dataset):
@@ -60,7 +74,7 @@ class ImagesDataset(torch.utils.data.Dataset):
             else:
                 self.df = df_test
 
-        self.transform = get_transform(config, split)
+        self.dict_transforms = get_augs(config, split)
 
     def __len__(self):
         return self.df.shape[0]
@@ -68,25 +82,23 @@ class ImagesDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
         image = PIL.Image.open(self.path_images / row["name"])
-
-        # x_orig = self.transform(image)
-        # x = transforms.ToTensor()(x_orig)
-
         x_orig = transforms.ToTensor()(image)
-        x = self.transform(x_orig)
+        y_orig = [row[col_name] for col_name in ['squares', 'circles', 'up', 'right', 'down', 'left']]
 
-        y = [row[col_name] for col_name in ['squares', 'circles', 'up', 'right', 'down', 'left']]
-        y = torch.tensor(y).to(torch.int32)
+
+        x = self.dict_transforms["rotation_90"].transform_x(x_orig)
+        y = self.dict_transforms["rotation_90"].transform_y(y_orig)
 
         sample = {
             "x": x,
-            "y": y,
+            "y": torch.tensor(y).to(torch.int32),
             "filename": row['name']
         }
 
         debug = True
         if debug:
             sample["x_orig"] = x_orig
+            sample["y_orig"] = torch.tensor(y_orig).to(torch.int32)
 
         return sample
 
@@ -100,7 +112,7 @@ if __name__ == '__main__':
     import torch
 
     # TODO rozwiaz problem 4-ego kanalu na wczesniejszym etapie
-    # TODO sprawdzic, w jakiej libce sa zaimplementowane te augmentacje z timm.
+    # TODO 1 add y_orig.  There will be two "axes". x_orig with y_orig and x with y.
     def plot_tensor_as_image(tensor, orig_tensor, labels, filename):
         # Ensure both tensors are on the CPU and convert to numpy
         tensor = tensor.cpu().numpy()
@@ -150,5 +162,3 @@ if __name__ == '__main__':
             # break
 
     _display_dataset()
-
-# TODO znajdz miejsce na augmentacje
