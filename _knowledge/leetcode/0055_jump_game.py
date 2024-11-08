203613d8459b5dcd54913002f3456a9f27e30af0
driven12 <pawel.brysch.2@proton.me>
2024-11-08 06:25:15 +0100

    nie dziala jeden przypadek

--- _knowledge/leetcode/0055_jump_game.py
+++ _knowledge/leetcode/0055_jump_game.py
@@ -8,45 +8,38 @@ class Solution:
             else:
                 return False
 
-        left_idx = 0
         right_idx = 0
 
-        list_segments = []
+        # last    - len(nums) - 1
+        # last-1  - len(nums) - 2
+        # last-2  - len(nums) - 3
+
         while right_idx <= len(nums) - 3:
             right_idx += 1
-            if nums[right_idx+1] != 0 and nums[right_idx] == 0:
-                list_segments.append(nums[left_idx:(right_idx+1)])
-                left_idx = right_idx + 1
-                right_idx = left_idx
 
-        list_segments.append(nums[left_idx:(right_idx+1)])
-        list_bools = [self.is_segment_doable(segment) for segment in list_segments]
+            # print("right", nums[right_idx])
 
-        print(list_segments)
-        print(list_bools)
+            if (nums[right_idx + 1] != 0 and nums[right_idx] == 0) or (right_idx == len(nums) - 2):
+                left_idx = right_idx
+                while left_idx >= 0:
+                    left_idx -= 1
 
-        if False in list_bools:
-            return False
-        else:
-            return True
+                    if left_idx == -1:
+                        return False
 
-    def is_segment_doable(self, segment: List[int]) -> bool:
-        if 0 not in segment:
-            return True
+                    # print("right", nums[right_idx], "left", nums[left_idx])
 
-        for idx, lenght in enumerate(segment):
-            target_distance = len(segment) - idx
-            if lenght >= target_distance:
-                return True
+                    target_distance = right_idx - left_idx + 1
+                    length = nums[left_idx]
+                    if length >= target_distance:
+                        break
+        return True
 
-        return False
+print(Solution().canJump(nums = [2,3,1,1,4]))
+print(Solution().canJump(nums = [3,2,1,0,4]))
+print(Solution().canJump(nums = [3,2,1,0, 4,5,5,0]))
+print(Solution().canJump(nums = [3,0,  2,1,0,0,  2,3,1,1,4]))
 
-# print(Solution().canJump(nums = [2,3,1,1,4]))
-# print(Solution().canJump(nums = [3,2,1,0,4]))
-# print(Solution().canJump(nums = [3,2,1,0, 4,5,5,0]))
-# print(Solution().canJump(nums = [3,0,  2,1,0,0,  2,3,1,1,4]))
-
-# print(Solution().canJump(nums = [0,2,3]))
 print(Solution().canJump(nums = [
     8,2,4,4,4,9,5,2,5,8,8,0,
     8,6,9,1,1,6,3,5,1,2,6,6,0,
@@ -61,3 +54,6 @@ print(Solution().canJump(nums = [
     8,2,5,1,2,3,9,7,4,7,0,0,
     1,8,5,6,7,5,1,9,9,3,5,0,
     7,  5]))
+
+print(Solution().canJump(nums = [2,1,0,0]))
+print(Solution().canJump(nums =   [1,2,3]))
