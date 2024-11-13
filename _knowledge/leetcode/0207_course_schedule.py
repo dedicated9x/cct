6ec3a915da57810e3453766e1e26167a141ddffd
driven12 <pawel.brysch.2@proton.me>
2024-11-13 16:37:44 +0100

    chyba niefortunna implementacja usuwacza galezi

--- _knowledge/leetcode/0207_course_schedule.py
+++ _knowledge/leetcode/0207_course_schedule.py
@@ -1,10 +1,38 @@
 from typing import List
-from collections import deque
 
 class Node:
-    def __init__(self, val = 0, targets = None):
+    def __init__(self, val = 0):
         self.val = val
-        self.targets = targets if targets is not None else []
+        self.edges_in = []
+        self.edges_out = []
+        self.type = None
+        self.is_removable = None
+
+    def __str__(self):
+        return f"{self.val} in:{self.edges_in} out:{self.edges_out} type:{self.type}"
+
+    def calculate_flags(self):
+        self._calculate_type()
+        self._evaluate_is_removable()
+
+    def _calculate_type(self):
+        len_in = len(self.edges_in)
+        len_out = len(self.edges_out)
+
+        if len_in > 0 and len_out > 0:
+            self.type = "bidirectional"
+        elif len_in > 0 and len_out == 0:
+            self.type = "all_in"
+        elif len_in == 0 and len_out > 0:
+            self.type = "all_out"
+        else:
+            self.type = "isolated"
+
+    def _evaluate_is_removable(self):
+        if self.type in ["all_in", "all_out"]:
+            self.is_removable = True
+        else:
+            self.is_removable = False
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -12,35 +40,39 @@ class Solution:
             return True
 
         list_nodes = [Node(val=idx) for idx in range(numCourses)]
-        for idx_node, idx_target in prerequisites:
-            list_nodes[idx_node].targets.append(list_nodes[idx_target])
+        for edge in prerequisites:
+            node_out, node_in = edge
 
-        for idx_start, _ in enumerate(list_nodes):
-            if self.has_loop(idx_start, list_nodes):
-                return False
+            # It's better to have hashable elements.
+            edge = (node_out, node_in)
 
-        # Doesn't have any loops.
-        return True
+            list_nodes[node_out].edges_out.append(edge)
+            list_nodes[node_in].edges_in.append(edge)
 
-    def has_loop(self, idx_start, list_nodes) -> bool:
-        start_node = list_nodes[idx_start]
+        for node in list_nodes:
+            node.calculate_flags()
 
-        d = deque()
-        for node in start_node.targets:
-            d.append(node)
-        trawersed_nodes = []
-        while d:
-            next_node = d.popleft()
-            trawersed_nodes.append(next_node)
-            for node in next_node.targets:
-                if (node not in trawersed_nodes) and (node not in d):
-                    d.append(node)
+        while any([e.is_removable for e in list_nodes]):
+            for node in list_nodes:
+                if node.type == "all_out":
+                    a = 2
+        # str(list_nodes[0])
 
-        if start_node in trawersed_nodes:
-            return True
-        else:
-            return False
 
-print(Solution().canFinish(numCourses = 2, prerequisites = [[1,0]]))
-print(Solution().canFinish(numCourses = 2, prerequisites = [[1,0],[0,1]]))
-print(Solution().canFinish(numCourses = 6, prerequisites = [[0,1], [1,2], [2, 0], [4, 3], [5,3]]))
+print(Solution().canFinish(numCourses = 11, prerequisites = [
+    [0, 7],
+    [1, 3],
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [5, 6],
+    [6, 4],
+    [5, 7],
+    [6, 8],
+    [6, 10],
+    [8, 9],
+    [10, 9],
+]))
+# print(Solution().canFinish(numCourses = 2, prerequisites = [[1,0]]))
+# print(Solution().canFinish(numCourses = 2, prerequisites = [[1,0],[0,1]]))
+# print(Solution().canFinish(numCourses = 6, prerequisites = [[0,1], [1,2], [2, 0], [4, 3], [5,3]]))
