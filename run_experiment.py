9c154946f44afbaad9cb3c2a848c13166b96515b
Pawel Brysch <pawbrysch@task.gda.pl>
2026-04-15 14:53:43 +0200

    Przepiecie na 00_shapes_base

--- run_experiment.py
+++ run_experiment.py
@@ -16,7 +16,8 @@ gsn1/conf/01_counts_base -> 0.26, 0.41, 0.52
 # @hydra.main(version_base="1.2", config_path="src/tasks/gsn1/conf", config_name="01_counts_base")
 # @hydra.main(version_base="1.2", config_path="src/tasks/gsn1/conf", config_name="02_counts_encoded_base")
 # @hydra.main(version_base="1.2", config_path="src/tasks/gsn2/conf", config_name="00_base")
-@hydra.main(version_base="1.2", config_path="src/tasks/molecule/conf", config_name="00_base")
+# @hydra.main(version_base="1.2", config_path="src/tasks/molecule/conf", config_name="00_base")
+@hydra.main(version_base="1.2", config_path="src/tasks/gsn1/conf", config_name="00_shapes_base")
 def main(config: omegaconf.DictConfig) -> None:
     run_experiment(config)
 
@@ -27,7 +28,7 @@ def run_experiment(config: omegaconf.DictConfig) -> None:
 
     # config.trainer.ckpt_path = "/tmp/wandb_pl_logs/cct/default/version_3/checkpoints/14-0.97.ckpt"
     # config.trainer.max_epochs = 40
-    config.trainer.max_epochs = 10
+    # config.trainer.max_epochs = 10
     # config.trainer.batch_size = 6
 
 
