364cf3ebeb7955e4ff43c750c5e3409da6789201
Pawel Brysch <pawbrysch@task.gda.pl>
2026-04-16 09:49:35 +0200

    MILESTONE Nie ma juz common

--- src/module.py
+++ src/module.py
@@ -1,15 +1,14 @@
-from pathlib import Path
 import torch
-import numpy as np
+import pytorch_lightning as pl
 
-from src.common.module import BaseModule
 from src.arch import ShapeClassificationNet
 from src.dataset import ImagesDataset
 from src.metrics import bcewithlogits_multilabel, convert_topk_to_binary
 
-class ShapesModule(BaseModule):
+class ShapesModule(pl.LightningModule):
     def __init__(self, config=None):
-        super(ShapesModule, self).__init__(config)
+        super(ShapesModule, self).__init__()
+        self.config = config
 
         self.model = ShapeClassificationNet(
             out_features=6,
@@ -22,6 +21,37 @@ class ShapesModule(BaseModule):
         self.ds_test = ImagesDataset(config, "test")
 
         self.save_hyperparameters(config)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.ds_train,
+            batch_size=self.config.trainer.batch_size,
+            shuffle=True,
+            num_workers=4,
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.ds_val,
+            batch_size=self.config.trainer.batch_size,
+            num_workers=4,
+        )
+
+    def test_dataloader(self):
+        if self.config.main.is_tested:
+            return torch.utils.data.DataLoader(
+                self.ds_test,
+                batch_size=self.config.trainer.batch_size,
+                num_workers=4,
+            )
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.optimizer.lr)
+        return optimizer
+
 
     def training_step(self, batch, batch_idx):
         x, targets = batch['x'], batch['y_shapes']
